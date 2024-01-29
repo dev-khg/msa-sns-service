80b3baa958b2a08006b09a6d956a79bad9180b7a
HongGeun <ghdrms163@gmail.com>
2024-01-29 16:27:59 +0900

    test 환경 분리

--- src/test/java/com/example/preorder/test_config/EmbeddedRedisConfig.java
+++ src/test/java/com/example/preorder/test_config/EmbeddedRedisConfig.java
@@ -1,16 +1,14 @@
-package com.example.preorder.global.redis;
+package com.example.preorder.test_config;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
-@Slf4j
-@Configuration
-@Profile("test")
+@TestConfiguration
 public class EmbeddedRedisConfig {
     private final RedisServer redisServer;
 
@@ -23,7 +21,6 @@ public class EmbeddedRedisConfig {
         try {
             redisServer.start();
         } catch (Exception e) {
-            log.error("Can't start embedded redis server.\nmessage = {}", e.getMessage());
         }
     }
 
@@ -32,3 +29,4 @@ public class EmbeddedRedisConfig {
         redisServer.stop();
     }
 }
+
