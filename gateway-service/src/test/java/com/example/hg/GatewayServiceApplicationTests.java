d7db97d83be6d4d15d446c100abf7002ee5596eb
HongGeun <ghdrms163@gmail.com>
2024-01-30 14:59:45 +0900

    Update GatewayServiceApplicationTests.java

--- gateway-service/src/test/java/com/example/hg/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/example/hg/GatewayServiceApplicationTests.java
@@ -3,10 +3,8 @@ package com.example.hg;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GatewayServiceApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
