18a56d87d85a34590d3750cc7a2851a985c31105
HongGeun <ghdrms163@gmail.com>
2024-01-31 15:53:58 +0900

    init - add news feed service

---
+++ newsfeed-service/src/test/java/com/example/newsfeedservice/NewsfeedServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.newsfeedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class NewsfeedServiceApplicationTests {
+
+}
